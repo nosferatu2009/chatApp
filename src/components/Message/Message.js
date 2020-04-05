88f7dd3ebaf444159188580aeb8b641676c4e734
nosferatu2009 <51450808+nosferatu2009@users.noreply.github.com>
2020-04-05 18:07:24 +0100

    Add files via upload

---
+++ src/components/Message/Message.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Message.css'
+import ReactEmoji from 'react-emoji'
+
+const Message = ( { message: { user, text}, name }) => {
+  
+    let isSenttByCurrentUser = false;
+
+    const trimmedName = name.trim().toLowerCase();
+
+    if(user === trimmedName ){
+        isSenttByCurrentUser = true;
+    }
+
+    return(
+        isSenttByCurrentUser 
+        ? ( <div className="messageContainer justifyEnd">
+               <p className="sendText pr-10">{ trimmedName }</p>
+               <div className="messageBox backgroundBlue">
+                 <p className="messageText colorWhite"> { ReactEmoji.emojify(text)}</p>
+            </div>
+        </div>
+    ) : (
+        <div className="messageContainer justifyStart">
+            <div className="messageBox backgroundLight">
+                <p className="messageText colorDark"> { ReactEmoji.emojify(text) }</p>
+            </div>
+            <p className="sendText pl=10">{ user }</p>
+        </div>
+
+    )
+   
+  );   
+}
+
+export default Message;
